7a7edd74322bee4417f669c20fd806fc1b99fd84
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-09-19 11:04:25 +0900

    esling

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    ignorePatterns: ['node_modules', '.next'],
+    extends: ['next', 'prettier'],
+    ecmaVersion, sourceType,
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 'warn',
+        'no-unused-vars': 'warn',
+        // Next.js では React import が不要なので無効化
+        'react/react-in-jsx-scope': 'off',
+    },
+};
