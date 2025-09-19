de22cf23f50f5b62e6bb859b4906d423256d2767
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-09-19 11:33:41 +0900

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
