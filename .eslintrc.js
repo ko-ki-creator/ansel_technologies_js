9012fca6daf1cd8056337dde18e8a8d1904a46c7
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-09-20 18:51:06 +0900

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     rules: {
         'no-console': 'warn',
         'no-unused-vars': 'warn',
-        // Next.js では React import が不要なので無効化
         'react/react-in-jsx-scope': 'off',
     },
 };
